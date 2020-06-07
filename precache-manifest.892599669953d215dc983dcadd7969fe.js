f4a3359381f0fcec4273f11d87c3dfd6d6efcfcf
Choton <choton654@gmail.com>
2020-06-07 22:31:17 +0530

    Updates

---
+++ precache-manifest.892599669953d215dc983dcadd7969fe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9146369733635af9091e17e971f18e79",
+    "url": "/covid-19_react/index.html"
+  },
+  {
+    "revision": "a639bafdbbe42a853007",
+    "url": "/covid-19_react/static/css/main.84fe114c.chunk.css"
+  },
+  {
+    "revision": "60d2d1175447f2c7fb99",
+    "url": "/covid-19_react/static/js/2.058139bf.chunk.js"
+  },
+  {
+    "revision": "44e39e61fb6a0450f7c8014318774752",
+    "url": "/covid-19_react/static/js/2.058139bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a639bafdbbe42a853007",
+    "url": "/covid-19_react/static/js/main.6693c95b.chunk.js"
+  },
+  {
+    "revision": "7f5b972cf3e454fd19b6",
+    "url": "/covid-19_react/static/js/runtime-main.ccab03af.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid-19_react/static/media/image.d7265326.png"
+  }
+]);
